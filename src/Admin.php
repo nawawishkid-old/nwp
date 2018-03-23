36a33c372a4dbae5798fb2b70b98e7003a95b62c
Nawawish <hydrokidz@gmail.com>
2018-03-24 00:10:06 +0700

    Update Admin.php

--- src/Admin.php
+++ src/Admin.php
@@ -13,17 +13,70 @@ class Admin {
 	private $styles = [];
 	private $dashboard_widgets = [];
 	private $pages = [];
-	private $widgets = [];
 
+	/**
+	 * @var array
+	 */
+	private $widgets = [];
+	
+	/**
+	 * @var array
+	 */
+	private $hiddenWidgets = [];
+
+	/**
+	 * @var bool
+	 */
+	private $noWidget = false;
+
+	/**
+	 *
+	 * @return void
+	 */
 	public function build() {
 		if ( !empty( $this->widgets ) )
-			\add_action( 'widgets_init', [$this, 'buildWidgets'] );
+			\add_action( 'widgets_init', [$this, '_addWidgets'], 10 );
+
+		if ( !empty( $this->hiddenWidgets ) || $this->noWidget )
+			\add_action( 'widgets_init', [$this, '_hideWidgets'], 20 );
 	}
 
-	public function buildWidgets() {
+	/**
+	 *********************
+	 * Widgets Section
+	 *********************
+	 */
+	/**
+	 *
+	 *
+	 * @return $this
+	 */
+	public function _addWidgets() {
 		foreach ( $this->widgets as $widget ) {
 			\register_widget( $widget );
 		}
+
+		return $this;
+	}
+
+	public function _hideWidgets() {
+		// Hide all widgets
+		if ( $this->noWidget ) {
+			global $wp_widget_factory;
+
+			if ( empty( $wp_widget_factory->widgets ) )
+				return;
+
+			$wp_widget_factory->widgets = [];
+
+			return;
+		}
+
+		// Hide specific widget
+		foreach ( $this->hiddenWidgets as $widget ) {
+			\unregister_widget( $widget );
+		}
+
 	}
 
 	public function addWidgets( array $widgets ) {
@@ -34,10 +87,63 @@ class Admin {
 		return $this;
 	}
 
+	/**
+	 * Add given widget to $this->widgets
+	 *
+	 * @param NPW\Widget $widget Widget to be added
+	 * @return $this
+	 */
 	public function addWidget( Widget $widget ) {
 		$this->widgets[] = $widget;
 		return $this;
 	}
+
+	/**
+	 * Hide specific registered widget.
+	 *
+	 * @see https://core.trac.wordpress.org/browser/tags/4.9.4/src/wp-includes/widgets.php#L134
+	 *
+	 * @param string | NWP\Widget $widget Name of WP_Widget subclass or NWP|Widget instance
+	 * @return $this
+	 */
+	public function hideWidget( $widget ) {
+		if ( ! is_string( $widget ) && ! $widget instanceof Widget )
+			throw new \InvalidArgumentException("Given widget is invalid.");
+
+		$this->hiddenWidgets[] = $widget;
+
+		return $this;	
+	}
+
+	/**
+	 * Add specific widget to $this->hiddenWidgets to be used in $this->_hideWidgets()
+	 * or set $this->noWidget to true if null given.
+	 *
+	 * @param NULL | array $widgets Array of widgets or null if user wants to hide all registered widgets
+	 * @return $this
+	 */
+	public function hideWidgets( $widgets = null ) {
+		// Hide all widgets
+		if ( is_null( $widgets ) ) {
+			$this->noWidget = true;
+
+	    	return $this;
+		}
+
+		if ( ! is_array( $widgets ) )
+			throw new \InvalidArgumentException("Given parameter must be of type array.");
+
+		foreach ( $widgets as $widget ) {
+			$this->hideWidget( $widget );
+		}
+
+		return $this;
+	}
+	/**
+	 *********************
+	 * End of Widgets Section
+	 *********************
+	 */
 
 	/**
 	 *
